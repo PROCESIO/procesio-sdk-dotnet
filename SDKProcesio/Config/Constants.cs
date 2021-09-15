e1f1d2f1207f3f258856ae650626fa8987bc10bc
ana-musat <anamaria.musat@procesio.com>
2021-09-15 16:02:20 +0300

    Change version to 1.0.0 Update Constants URL for prod

--- SDKProcesio/Config/Constants.cs
+++ SDKProcesio/Config/Constants.cs
@@ -2,9 +2,9 @@
 {
     public static class Constants
     {
-        public const string ProcesioAuthURL = "https://dev.procesio.app:4532/";
+        public const string ProcesioAuthURL = "https://api.procesio.app:4532/";
         public const string ProcesioAuthMethod = "/api/Authentication";
-        public const string ProcesioURL = "https://dev.procesio.app:4321/";
+        public const string ProcesioURL = "https://api.procesio.app:4321/";
         public const string ProcesioRunMethod = "/api/Projects/{0}/run";
         public const string ProcesioPublishMethod = "/api/Projects/{0}/instances/publish";
         public const string ProcesioLaunchMethod = "/api/Projects/instances/{0}/launch";
