b5849cdc89da9e0d997219b49c834bdaae7c7cd7
Alexandru Gal <alexandru.gal@procesio.com>
2021-10-01 23:06:29 +0300

    Update constants

--- ProcesioSDK/Config/Constants.cs
+++ ProcesioSDK/Config/Constants.cs
@@ -7,7 +7,7 @@
         public const string PROCESIO_RUN_METHOD = "/api/Projects/{0}/run";
         public const string PROCESIO_PUBLISH_METHOD = "/api/Projects/{0}/instances/publish";
         public const string PROCESIO_LAUNCH_METHOD = "/api/Projects/instances/{0}/launch";
-        public const string PROCESIO_UPLOAD_FLOW_FILE = "/api/File/upload/flow";
+        public const string PROCESIO_UPLOAD_FLOW_FILE = "/api/file/upload/flow";
         public const string PROCESIO_FLOW_STATE = "/api/Projects/instances/{0}/status";
         public const string PROCESIO_FILE_DATA_TYPE_ID = "10c6ac59-3929-49e6-99dc-121212121219";
         public const string PROCESIO_FILE_DATA_PROPERTY_ID = "id";
